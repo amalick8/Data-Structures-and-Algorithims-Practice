ddf3589cfe54d4739546397683638b8fd8d2e167
Ammar Malick <ammarmalick2006@gmail.com>
2026-01-21 21:40:17 -0600

    Refactor count_good_substring function to improve clarity and functionality

--- count_good_substrings.py
+++ count_good_substrings.py
@@ -1,3 +1,18 @@
-def count_good_substrings(s,k):
-    for i in range()
-    initial_chunk = s[i:i+k]
+# A window is just "A group of characters we are currently looking at"
+# Fixed-size means: "The group is ALWAYS length k"
+
+def count_good_substring(string,k):
+    if len(string) < k:
+        return 0
+    
+    count = 0
+    window = string[0:k]
+    if len(set(window)) == len(window):
+        count += 1
+    
+    for i in range(k, len(string)):
+        window = window[1:] + string[i]
+        if len(set(window)) == k:
+            count += 1
+
+    return count
